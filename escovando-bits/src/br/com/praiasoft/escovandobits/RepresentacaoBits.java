db394e2a8bd762285fb149da2a15a351af049bbb
Rodrigo Jesus <rodrigo@praiasoft.com.br>
2020-12-01 23:16:16 -0300

    BitSet

--- escovando-bits/src/br/com/praiasoft/escovandobits/RepresentacaoBits.java
+++ escovando-bits/src/br/com/praiasoft/escovandobits/RepresentacaoBits.java
@@ -1,8 +1,26 @@
 package br.com.praiasoft.escovandobits;
 
+import java.util.BitSet;
+
 public class RepresentacaoBits {
 	public static void main(String[] args) {
-		display(123345678);
+		int inteiro = 123345678;
+		display( inteiro );
+		
+		System.out.println();
+		System.out.println(
+				String.format("%32s", Integer.toBinaryString(inteiro)).replace(" ", "0"));
+		
+		BitSet b1 = new BitSet();
+		b1.set(0);
+		b1.set(1);
+		b1.set(2);
+		b1.set(3);
+		b1.set(4);
+		b1.set(5);
+		b1.set(6);
+//		b1.set(7);
+		System.out.println(b1.toByteArray()[0]);
 	}
 
 	public static void display(int value) {
